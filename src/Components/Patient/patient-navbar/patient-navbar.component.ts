2ba5ff3e67c3d1991df9e19016dd7feeedcacfe6
islam fathy <eslamfathyelbasatini55a@gmail.com>
2026-05-31 17:16:31 +0300

    ai agent chat for doctor and patient

--- src/Components/Patient/patient-navbar/patient-navbar.component.ts
+++ src/Components/Patient/patient-navbar/patient-navbar.component.ts
@@ -80,6 +80,12 @@ export class PatientNavbarComponent {
     route: '/patient/ai',
     svgPath: 'M12 2a3 3 0 0 0-3 3v1H7a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h1v2H7a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h2v1a3 3 0 0 0 6 0v-1h2a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2h-1v-2h1a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2h-2V5a3 3 0 0 0-3-3zm-1 5h2v2h-2V7zm0 6h2v2h-2v-2z',
   },
+  {
+  label: 'Ask Agent',
+  hint: 'Ask AI any question',
+  route: '/patient/askagent',
+  svgPath: 'M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z'
+}
   ];
 
   initials = computed(() =>
